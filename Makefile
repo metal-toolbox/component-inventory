3dd14c8588b06496e43318079a891fd55e687d32
Doctor Vince <DoctorVin@users.noreply.github.com>
2024-02-14 17:23:52 -0500

    initial CIS

---
+++ Makefile
@@ -0,0 +1,79 @@
+export DOCKER_BUILDKIT=1
+SERVICE_NAME ?= component-inventory
+GIT_COMMIT  := $(shell git rev-parse --short HEAD)
+GIT_BRANCH  := $(shell git symbolic-ref -q --short HEAD)
+GIT_SUMMARY := $(shell git describe --tags --dirty --always)
+VERSION     := $(shell git describe --tags 2> /dev/null)
+BUILD_DATE  := $(shell date +%s)
+GO_VERSION := $(shell expr `go version |cut -d ' ' -f3 |cut -d. -f2` \>= 20)
+LDFLAG_LOCATION := github.com/metal-toolbox/${SERVICE_NAME}/internal/version
+DOCKER_IMAGE  ?= ghcr.io/metal-toolbox/${SERVICE_NAME}
+SANDBOX_IMAGE ?= localhost:5001/${SERVICE_NAME}
+SANDBOX_TEMPLATE_DIR ?= ${HOME}/Development/sandbox/templates
+
+.DEFAULT_GOAL := help
+
+## lint
+lint:
+	golangci-lint run --config .golangci.yml
+
+## Go test
+test: lint
+	CGO_ENABLED=0 go test -timeout 1m -v -covermode=atomic ./...
+
+build: 
+	CGO_ENABLED=0 go build -o ${SERVICE_NAME} 
+
+clean:
+	rm -rf ${SERVICE_NAME}
+
+image:
+	docker build -t ${DOCKER_IMAGE}:latest . \
+		--build-arg APP_NAME=${SERVICE_NAME} \
+		--build-arg LDFLAG_LOCATION=${LDFLAG_LOCATION} \
+		--build-arg GIT_COMMIT=${GIT_COMMIT} --build-arg GIT_BRANCH=${GIT_BRANCH} \
+		--build-arg GIT_SUMMARY=${GIT_SUMMARY} --build-arg VERSION=${VERSION} \
+		--build-arg BUILD_DATE=${BUILD_DATE} 
+
+push-sandbox-image: image
+	docker tag ${DOCKER_IMAGE}:latest ${SANDBOX_IMAGE}:latest
+	docker push localhost:5001/${SERVICE_NAME}:latest
+	kind load docker-image localhost:5001/${SERVICE_NAME}:latest
+
+push-image: image
+	docker push ${DOCKER_IMAGE}:latest
+
+# if you want to use the sandbox repo, this target puts the files from this service in place. You will
+# need to update the sandbox Makefile to get helm to load everything properly.
+load-sandbox:
+	@cp helm/values.yaml "${SANDBOX_TEMPLATE_DIR}/cis-values.yaml"
+	@cp helm/service.yaml "${SANDBOX_TEMPLATE_DIR}/cis-service.yaml"
+	@cp helm/deployment.yaml "${SANDBOX_TEMPLATE_DIR}/cis-deployment.yaml"
+	@cp helm/configmap.yaml "${SANDBOX_TEMPLATE_DIR}/cis-configmap.yaml"
+	@echo "Be sure to do a helm (re)load to get the service started"
+
+# https://gist.github.com/prwhite/8168133
+# COLORS
+GREEN  := $(shell tput -Txterm setaf 2)
+YELLOW := $(shell tput -Txterm setaf 3)
+WHITE  := $(shell tput -Txterm setaf 7)
+RESET  := $(shell tput -Txterm sgr0)
+
+
+TARGET_MAX_CHAR_NUM=20
+## Show help
+help:
+	@echo ''
+	@echo 'Usage:'
+	@echo '  ${YELLOW}make${RESET} ${GREEN}<target>${RESET}'
+	@echo ''
+	@echo 'Targets:'
+	@awk '/^[a-zA-Z\-\\_0-9]+:/ { \
+		helpMessage = match(lastLine, /^## (.*)/); \
+		if (helpMessage) { \
+			helpCommand = substr($$1, 0, index($$1, ":")-1); \
+			helpMessage = substr(lastLine, RSTART + 3, RLENGTH); \
+			printf "  ${YELLOW}%-$(TARGET_MAX_CHAR_NUM)s${RESET} ${GREEN}%s${RESET}\n", helpCommand, helpMessage; \
+		} \
+	} \
+	{ lastLine = $$0 }' $(MAKEFILE_LIST)
---
+++ pkg/api/routes/routes.go
@@ -0,0 +1,188 @@
+package routes
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/metal-toolbox/component-inventory/internal/app"
+	"github.com/metal-toolbox/component-inventory/internal/metrics"
+	"github.com/metal-toolbox/component-inventory/internal/version"
+	"go.hollow.sh/toolbox/ginauth"
+	"go.hollow.sh/toolbox/ginjwt"
+	"go.uber.org/zap"
+)
+
+var (
+	readTimeout  = 10 * time.Second
+	writeTimeout = 20 * time.Second
+
+	authMiddleWare *ginauth.MultiTokenMiddleware
+	ginNoOp        = func(_ *gin.Context) {}
+)
+
+// apiHandler is a function that performs real work for this API.
+type apiHandler func(map[string]any) (map[string]any, error)
+
+func composeAppLogging(l *zap.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		// some evil middlewares modify this values
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+		c.Next() // call the next function in the chain
+		code := c.Writer.Status()
+		metrics.APICallEpilog(start, path, code)
+
+		fields := []zap.Field{
+			zap.String("path", path),
+			zap.String("query", query),
+			zap.Int("status-code", code),
+			zap.Time("start", start),
+		}
+
+		if len(c.Errors) > 0 {
+			fields = append(fields, zap.Strings("errors", c.Errors.Errors()))
+			l.Error("errors on API request",
+				fields...,
+			)
+			return
+		}
+
+		l.Info("api call complete", fields...)
+	}
+}
+
+// ComposeHTTPServer returns an http.Server that handles our API
+func ComposeHTTPServer(theApp *app.App) *http.Server {
+	if len(theApp.Cfg.JWTAuth) != 0 {
+		var err error
+		authMiddleWare, err = ginjwt.NewMultiTokenMiddlewareFromConfigs(theApp.Cfg.JWTAuth...)
+		if err != nil {
+			theApp.Log.Fatal(
+				"failed to initialize auth middleware",
+				zap.Error(err),
+			)
+		}
+	}
+
+	g := gin.New()
+
+	if !theApp.Cfg.DeveloperMode {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	// set up common middleware for logging and metrics
+	g.Use(composeAppLogging(theApp.Log), gin.Recovery())
+
+	// some boilerplate setup
+	g.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound,
+			gin.H{
+				"message": "invalid request - route not found",
+			},
+		)
+	})
+
+	// a liveness endpoint
+	g.GET("/_health/liveness", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"time": time.Now()})
+	})
+
+	g.GET("/api/version", func(c *gin.Context) {
+		c.JSON(http.StatusOK, version.Current())
+	})
+
+	g.POST("/api/echo",
+		composeAuthHandler(createScopes("response")), // auth handler
+		wrapAPICall(apiEcho))                         // api function, wrapped into middleware
+
+	g.POST("/api/error",
+		composeAuthHandler(createScopes("response")),
+		wrapAPICall(apiError))
+
+	// add other API endpoints to the gin Engine as required
+
+	return &http.Server{
+		Addr:         theApp.Cfg.ListenAddress,
+		Handler:      g,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+	}
+}
+
+// wrapAPICall is an adapter for any arbitrary code so that you can isolate your
+// logic from having to take gin-specific data structures and whatnot. It assumes
+// your API function takes a map[string]any and returns a JSON-serializable result
+// and an error. This function could be altered to pull any kind of parameter out
+// of the raw JSON input.
+func wrapAPICall(fn apiHandler) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var responseCode int
+
+		m := make(map[string]any)
+		if err := ctx.BindJSON(&m); err != nil {
+			ctx.JSON(http.StatusBadRequest, map[string]any{
+				"error": err.Error(),
+			})
+		}
+
+		obj, err := fn(m)
+		if err == nil {
+			responseCode = http.StatusOK
+		} else {
+			responseCode = http.StatusInternalServerError
+			obj = map[string]any{
+				"error": err.Error(),
+			}
+		}
+		ctx.JSON(responseCode, obj)
+	}
+}
+
+func composeAuthHandler(scopes []string) gin.HandlerFunc {
+	if authMiddleWare == nil {
+		return ginNoOp
+	}
+	return authMiddleWare.AuthRequired(scopes)
+}
+
+func createScopes(items ...string) []string {
+	s := []string{"write", "create"}
+	for _, i := range items {
+		s = append(s, fmt.Sprintf("create:%s", i))
+	}
+
+	return s
+}
+
+//nolint:unused
+func readScopes(items ...string) []string {
+	s := []string{"read"}
+	for _, i := range items {
+		s = append(s, fmt.Sprintf("read:%s", i))
+	}
+
+	return s
+}
+
+//nolint:unused
+func updateScopes(items ...string) []string {
+	s := []string{"write", "update"}
+	for _, i := range items {
+		s = append(s, fmt.Sprintf("update:%s", i))
+	}
+
+	return s
+}
+
+//nolint:unused
+func deleteScopes(items ...string) []string {
+	s := []string{"write", "delete"}
+	for _, i := range items {
+		s = append(s, fmt.Sprintf("delete:%s", i))
+	}
+
+	return s
+}
