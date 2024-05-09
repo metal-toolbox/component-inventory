f34c693a6a1ea40d276308b5d6f39f7fdc7bfb6f
Doctor Vince <DoctorVin@users.noreply.github.com>
2024-05-09 14:15:36 -0400

    refactor routes to use query parameters for inband

--- pkg/api/constants/constants.go
+++ pkg/api/constants/constants.go
@@ -1,9 +1,10 @@
 package constants
 
 const (
-	LivenessEndpoint           = "/_health/liveness"
-	VersionEndpoint            = "/api/version"
-	ComponentsEndpoint         = "/components"
-	InbandInventoryEndpoint    = "/inventory/in-band"
-	OutofbandInventoryEndpoint = "/inventory/out-of-band"
+	LivenessEndpoint   = "/_health/liveness"
+	VersionEndpoint    = "/api/version"
+	ComponentsEndpoint = "/components"
+	InventoryEndpoint  = "/inventory"
+	OutOfBandMode      = "outofband"
+	InBandMode         = "inband"
 )
